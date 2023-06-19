bce6d809173e60c0e2213e44fca78482d6634235
Jessica <fr_jess@icloud.com>
2023-06-19 15:53:16 +0200

    [add] location on snaps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,14 +17,16 @@ export class AppComponent implements OnInit {
       description: 'Apple al parco con la sua palletta',
       imageUrl:'https://scontent.fmxp5-1.fna.fbcdn.net/v/t1.18169-9/10440834_10204401506457317_3729761187197286767_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=4IidxSJHUDcAX-NfTyS&_nc_ht=scontent.fmxp5-1.fna&oh=00_AfAL5-skZzU3YqxzW3qhym-0NhljxCpJlyRiNMFJL81YGg&oe=64B266F6',
       createdDate: new Date(),
-      snaps: 0
+      snaps: 0,
+      location: 'Roma'
     };
     this.myOtherSnap = {
       title: 'Scott',
       description: 'Scott al parco',
       imageUrl: 'https://scontent.fmxp5-1.fna.fbcdn.net/v/t1.18169-9/10606527_10204409989869397_6562112433066392422_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=-LeUcxguTNAAX-jbGV6&_nc_ht=scontent.fmxp5-1.fna&oh=00_AfBM8r8aS5kSO14Gztk8SFScVOtN7LYNlO1Jn8Xqqirffw&oe=64B28D99',
       createdDate: new Date(),
-      snaps: 0
+      snaps: 0,
+      location: 'Roma'
     };
     this.myLastSnap = {
       title: 'Golden Gate Bridge',
