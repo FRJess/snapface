e12df48a1da9919c72208628d736c7a9dc5473aa
Jessica <fr_jess@icloud.com>
2023-06-27 17:15:26 +0200

    [edit] snap/unsnap with method

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
+import { FaceSnapService } from 'src/services/face-snaps.service';
 
 @Component({
   selector: 'app-face-snap',
@@ -10,16 +11,20 @@ export class FaceSnapComponent implements OnInit {
   @Input() faceSnap!: FaceSnap;
   buttonText!: string;
 
+  constructor(private faceSnapService: FaceSnapService) {
+
+  }
+
   ngOnInit() {
     this.buttonText = 'Oh Snap!';
   }
 
   onSnap(){
     if (this.buttonText === 'Oh Snap!') {
-      this.faceSnap.snaps++;
+      this.faceSnapService.snapFaceSnapById(this.faceSnap.id);
       this.buttonText = 'Oops, unsnap!';
     }else{
-      this.faceSnap.snaps--;
+      this.faceSnapService.unsnapFaceSnapById(this.faceSnap.id);
       this.buttonText = 'Oh Snap!';
 
     }
--- src/services/face-snaps.service.ts
+++ src/services/face-snaps.service.ts
@@ -7,6 +7,7 @@ import { FaceSnap } from "src/app/models/face-snap.model";
 export class FaceSnapService{
   faceSnaps: FaceSnap[] = [
     {
+      id: 1,
       title: 'Apple',
       description: 'Apple al parco con la sua palletta',
       imageUrl:'https://scontent.fmxp5-1.fna.fbcdn.net/v/t1.18169-9/10440834_10204401506457317_3729761187197286767_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=4IidxSJHUDcAX-NfTyS&_nc_ht=scontent.fmxp5-1.fna&oh=00_AfAL5-skZzU3YqxzW3qhym-0NhljxCpJlyRiNMFJL81YGg&oe=64B266F6',
@@ -15,6 +16,7 @@ export class FaceSnapService{
       location: 'Roma'
     },
     {
+      id: 2,
       title: 'Scott',
       description: 'Scott al parco',
       imageUrl: 'https://scontent.fmxp5-1.fna.fbcdn.net/v/t1.18169-9/10606527_10204409989869397_6562112433066392422_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=-LeUcxguTNAAX-jbGV6&_nc_ht=scontent.fmxp5-1.fna&oh=00_AfBM8r8aS5kSO14Gztk8SFScVOtN7LYNlO1Jn8Xqqirffw&oe=64B28D99',
@@ -23,16 +25,61 @@ export class FaceSnapService{
       location: 'Roma'
     },
     {
+      id: 3,
       title: 'Golden Gate Bridge',
       description: 'Il Golden Gate Bridge, San Francisco',
       imageUrl: 'https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
       createdDate: new Date(),
-      snaps: 0
-      }
+      snaps: 60
+      },
+      {
+        id: 4,
+        title: 'Neon',
+        description: 'Scritta a neon in un bar',
+        imageUrl:'https://images.unsplash.com/photo-1556035511-3168381ea4d4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80',
+        createdDate: new Date(),
+        snaps: 10,
+      },
+      {
+        id: 5,
+        title: 'Chicago al tramonto',
+        description: 'Una strada di Chicaco',
+        imageUrl: 'https://images.unsplash.com/photo-1521127149192-4438d6686c74?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2031&q=80',
+        createdDate: new Date(),
+        snaps: 0,
+        location: 'Chicago'
+      },
+      {
+        id: 6,
+        title: 'I Fori Imperiali',
+        description: 'Vista sui Fori',
+        imageUrl: 'https://images.unsplash.com/photo-1626285220266-e35bd313d05b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1035&q=80',
+        createdDate: new Date(),
+        snaps: 48,
+        location: 'Roma'
+        }
   ];
 
   getAllFaceSnaps(): FaceSnap[] {
     return this.faceSnaps;
   }
+
+  snapFaceSnapById(faceSnapId: number): void {
+    const faceSnap = this.faceSnaps.find(faceSnap => faceSnap.id === faceSnapId)
+    if(faceSnap) {
+      faceSnap.snaps++;
+    } else {
+      throw new Error('FaceSnap not found');
+    }
+  }
+
+  unsnapFaceSnapById(faceSnapId: number): void {
+    const faceSnap = this.faceSnaps.find(faceSnap => faceSnap.id === faceSnapId);
+    if (faceSnap) {
+        faceSnap.snaps--;
+    } else {
+        throw new Error('FaceSnap not found!');
+    }
+}
 
 }
